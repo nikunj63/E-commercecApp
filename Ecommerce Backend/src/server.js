8b301afba3af73cdebec249276a136dd9de135e2
nikunj63 <nikunjs6324@gmail.com>
2024-10-20 21:10:26 +0530

    initial commit

---
+++ Ecommerce Backend/src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cors());
+
+mongoose.connect("mongodb+srv://Nikunj:1234@cluster0.yxv4y.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0");
+
+
+
+
+const PORT = 5001;
+app.listen(PORT, () => console.log('Server started at PORT : 5001'));
