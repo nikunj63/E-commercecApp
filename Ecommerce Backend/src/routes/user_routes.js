a4c6ea826728108ef6c63a9208e7f6c251f1419f
nikunj63 <nikunjs6324@gmail.com>
2024-11-02 17:59:02 +0530

    renamed

--- Ecommerce Backend/src/routes/user_routes.js
+++
@@ -1,7 +0,0 @@
-const UserRoutes = require('express').Router();
-const UserController = require('./../controllers/user_controller')
-
-UserRoutes.post("/createAccount",UserController.createAccount );
-UserRoutes.post("/SignIn",UserController.signIn);
-
-module.exports = UserRoutes; 
