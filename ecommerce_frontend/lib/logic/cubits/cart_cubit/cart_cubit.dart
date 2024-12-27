155c4a993a301fa0ac8807dee70df89ab40f72eb
nikunj63 <nikunjs6324@gmail.com>
2024-12-27 20:18:00 +0530

    addCartCubit

--- ecommerce_frontend/lib/logic/cubits/cart_cubit/cart_cubit.dart
+++ ecommerce_frontend/lib/logic/cubits/cart_cubit/cart_cubit.dart
@@ -1,4 +1,6 @@
 
+import 'dart:async';
+
 import 'package:ecommerce_frontend/logic/cubits/user_cubit/user_state.dart';
 
 import '../../../Data/repositeries/cart_repository.dart';
@@ -8,14 +10,24 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartCubit extends Cubit<CartState> {
   final UserCubit _userCubit;
+  StreamSubscription? _userSubscription;
+
   CartCubit (this._userCubit) : super( CartInitialState() ) {
-    // Listening to userCubit
-    _userCubit.stream.listen((state){
-      if(state is UserLoggedInState){
-        _initialize(state.userMdoel.sId!);
-      }
-    });
+    // initial value
+    _handleUserState(_userCubit.state);
+
+    // Listening to userCubit (for future updates)
+    _userSubscription =_userCubit.stream.listen(_handleUserState);
 }
+
+  void _handleUserState(UserState userState){
+    if(userState is UserLoggedInState){
+        _initialize(userState.userModel.sId!);
+      }
+      else if(userState is UserLoggedOutState){
+        emit(CartInitialState());
+      }
+  }
 
   final _cartRepository = CartRepository();
   void _initialize(String userId) async {
@@ -28,4 +40,10 @@ class CartCubit extends Cubit<CartState> {
     emit( CartErrorState (ex.toString(), state.items) );
       }
     }
+
+    @override
+  Future<void> close() {
+    _userSubscription?.cancel();
+    return super.close();
+  }
 }
--- ecommerce_frontend/lib/presentation/screens/cart/cart_screen.dart
+++ ecommerce_frontend/lib/presentation/screens/cart/cart_screen.dart
@@ -1,5 +1,8 @@
+import 'package:ecommerce_frontend/logic/cubits/cart_cubit/cart_cubit.dart';
+import 'package:ecommerce_frontend/logic/cubits/cart_cubit/cart_state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:input_quantity/input_quantity.dart';
 
 import '../../../core/ui.dart';
@@ -21,62 +24,85 @@ class _CartScreenState extends State<CartScreen> {
         title: Text("Cart"),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount:5 ,
-                itemBuilder: (context,index){
-                  return ListTile(
-                    // leading
-                  title: Text("Product Name"),
-                  subtitle: Text("price x quantity = total"),
-                  trailing: InputQty(
-                    minVal: 1,
-                    initVal: 2, // product initial quantity
-                    maxVal: 99,
-                    onQtyChanged: (value){},
-                  ),
-                  );
-                },
-              
-              ),
-            ),
+        child: BlocBuilder<CartCubit , CartState>(
+          builder: (context , state) {
 
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-              
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-              
-                        Text("5 items",style: TextStyles.body1.
-                        copyWith(fontWeight: FontWeight.bold),),
-                        Text("Total: 99895",style: TextStyles.heading3)
-                      ],
-                    )
-                    ),
-              
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width/2.5,
-                      child: CupertinoButton(
-                        onPressed: (){},
-                        padding:  EdgeInsets.all(MediaQuery.of(context).
-                        size.width/22),
-                        color: AppColors.accent,
-                        child: const Text("Place Order", style: TextStyle(
-                          color: Colors.white,
-                        ),),
+            if (state is CartLoadingState && state.items.isEmpty) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (state is CartErrorState && state.items.isEmpty) {
+              return Center(
+                child: Text(state.message)
+              );
+            }
+
+
+
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount:state.items.length ,
+                    itemBuilder: (context,index){
+
+                      final item = state.items[index];
+
+
+                      return ListTile(
+                        // leading
+                      title: Text("${item.product?.title}"),
+                      subtitle: Text("price x quantity = total"),
+                      trailing: InputQty(
+                        minVal: 1,
+                        initVal: 2, // product initial quantity
+                        maxVal: 99,
+                        onQtyChanged: (value){},
                       ),
-                    )
-                ],
-              ),
-            )
-          ],
+                      );
+                    },
+                  
+                  ),
+                ),
+            
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                  
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                  
+                            Text("5 items",style: TextStyles.body1.
+                            copyWith(fontWeight: FontWeight.bold),),
+                            Text("Total: 99895",style: TextStyles.heading3)
+                          ],
+                        )
+                        ),
+                  
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width/2.5,
+                          child: CupertinoButton(
+                            onPressed: (){},
+                            padding:  EdgeInsets.all(MediaQuery.of(context).
+                            size.width/22),
+                            color: AppColors.accent,
+                            child: const Text("Place Order", style: TextStyle(
+                              color: Colors.white,
+                            ),),
+                          ),
+                        )
+                    ],
+                  ),
+                )
+              ],
+            );
+          }
         ),
         ),
     );
